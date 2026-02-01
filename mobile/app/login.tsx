ae8d523780ce94b97c37ad3f474bc01cc78e9a27
ShehabM0 <shehabmohamed2357@gmail.com>
2026-02-01 22:50:52 +0300

    add LoginScreen

---
+++ mobile/app/login.tsx
@@ -0,0 +1,172 @@
+import { Colors } from '@/components/common/colors';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import React, { useState } from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      Alert.alert('Error', 'Please fill in all fields');
+      return;
+    }
+
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 5000); 
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <View style={styles.header}>
+        <Text style={styles.headTitle}>PassKey<Text style={{color: 'blue'}}>+</Text></Text>
+      </View>
+
+      <View style={styles.content}>
+
+        <Text style={styles.title}>Welcome Back</Text>
+        <Text style={styles.subtitle}>Sign in to continue</Text>
+
+        <View style={styles.form}>
+          <View style={styles.inputContainer}>
+            <MaterialIcons name="email" size={24} color="black" />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              value={email}
+              onChangeText={setEmail}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              editable={!isLoading}
+            />
+          </View>
+
+          <View style={styles.inputContainer}>
+            <MaterialIcons name="lock" size={24} color="black" />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+              editable={!isLoading}
+            />
+          </View>
+
+          <TouchableOpacity
+            style={[styles.button, isLoading && styles.buttonDisabled]}
+            onPress={handleLogin}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <ActivityIndicator color={Colors.white} />
+            ) : (
+              <Text style={styles.buttonText}>Login</Text>
+            )}
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.forgotPassword}>
+            <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.forgotPassword}>
+            <Text style={styles.forgotPasswordText}>Don't have an account? <Text style={styles.signUpText}>Sign up</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.gray100
+  },
+  header: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 48 : 32,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  content: {
+    flex: 3,
+    paddingHorizontal: 24
+  },
+  headTitle: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: Colors.gray900
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: Colors.gray900
+  },
+  subtitle: {
+    fontSize: 16,
+    color: Colors.gray500,
+    marginBottom: 40,
+  },
+  form: {
+    width: '100%',
+  },
+  input: {
+      flex: 1,
+      height: '100%',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    height: 50,
+    backgroundColor: '#f1f1f1',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: Colors.black,
+    borderRadius: 8,
+    padding: 16,
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  buttonDisabled: {
+    backgroundColor: Colors.gray300
+  },
+  buttonText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  forgotPassword: {
+    marginTop: 16,
+    alignItems: 'center',
+  },
+  forgotPasswordText: {
+    color: Colors.gray700,
+    fontSize: 14,
+  },
+  signUpText: {
+    textDecorationLine: 'underline',
+    fontWeight: 'bold'
+  },
+});
---
+++ mobile/components/common/colors.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  black: '#0B0B0B',
+  white: '#FFFFFF',
+
+  gray100: '#F7F7F7',    // Backgrounds
+  gray200: '#E5E5E5',    // Borders / dividers
+  gray300: '#D4D4D4',    // Disabled
+  gray500: '#737373',    // Secondary text
+  gray700: '#404040',    // Body text
+  gray900: '#171717',    // Headings
+
+  error: '#DC2626',      // Errors only
+};
